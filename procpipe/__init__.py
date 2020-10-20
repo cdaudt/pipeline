03d54b9daca76c453f9062da4f8cb25bb7d0f011
Christian Daudt <christian@daudt.org>
2020-10-20 13:31:27 -0700

    fix header

--- procpipe/__init__.py
+++ procpipe/__init__.py
@@ -1,6 +1,5 @@
-# Simple example to show usage of the data processing pipes
-# Parses through an array of words and prints out the ones >= 5 chars
-# See https://github.com/cdaudt/pipeline for more details
+# Part of procpipe python module
+# See https://github.com/cdaudt/pipeline
 # Author: Christian Daudt
 
 from .version import __version__
